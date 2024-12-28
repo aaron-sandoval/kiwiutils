969f53748659e1ebed439115a7f0959f12e9ab5f
aaron-sandoval <32021231+aaron-sandoval@users.noreply.github.com>
2024-12-27 17:50:38 -0700

    Copy finite_valued from maze-dataset, update deps

--- kiwiutils/kiwilib.py
+++ kiwiutils/kiwilib.py
@@ -99,6 +99,18 @@ def flatten(it: Iterable, numLevels: int = pd.NA) -> Generator:
             yield x
 
 
+def is_abstract(cls: type) -> bool:
+    """
+    Returns if a class is abstract.
+    """
+    if not hasattr(cls, "__abstractmethods__"):
+        return False  # an ordinary class
+    elif len(cls.__abstractmethods__) == 0:
+        return False  # a concrete implementation of an abstract class
+    else:
+        return True  # an abstract class
+
+
 def getAllSubclasses(class_: type, includeSelf=False) -> Set[type]:
     """
     Returns a set containing all child classes in the subclass graph of `class_`.
